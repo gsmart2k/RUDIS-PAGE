14eea383e12ef29824c827c58ebc157d2c512b42
Gbolahan <ogunrinadegbolahan2000@gmail.com>
2022-02-05 19:23:08 +0100

    css changes

--- public/script/main.js
+++ public/script/main.js
@@ -1,22 +1,21 @@
-const ham = document.querySelector(".ham")
-const navlinks = document.querySelector(".nav-links")
+/** @format */
 
+const ham = document.querySelector(".ham");
+const navlinks = document.querySelector(".nav-links");
 
-ham.addEventListener("click", openNav)
+ham.addEventListener("click", openNav);
 
-
-function openNav(){
-    if(navlinks.classList.contains("hide")){
-        navlinks.classList.remove("hide")
-        navlinks.classList.add("show")
-    }else{
-        navlinks.classList.remove("show")
-        navlinks.classList.add("hide")
-    }
+function openNav() {
+  if (navlinks.classList.contains("hide")) {
+    navlinks.classList.remove("hide");
+    navlinks.classList.add("show");
+  } else {
+    navlinks.classList.remove("show");
+    navlinks.classList.add("hide");
+  }
 }
 // function closeNav(){
 //     nav.classList.remove("show")
 //     parentham1.classList.add("show")
 //     parentham2.classList.add("hide")
 // }
-
